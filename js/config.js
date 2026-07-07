e9b88555905ef682c6f87b96d47fde963d188f4b
rem <rem@s-rem.jp>
2026-07-07 23:27:01 +0900

    0.6.0 Change Looks and Responsive movie list

--- js/config.js
+++ js/config.js
@@ -18,8 +18,8 @@ const APP_CONFIG = {
 
     name: "Filmarks Dashboard",
 
-    version: "v0.5.22",
+    version: "v0.6.0",
 
-    latest: "Adjust movie table height dynamically",
+    latest: "Change Looks and Responsive movie list",
 
 };
